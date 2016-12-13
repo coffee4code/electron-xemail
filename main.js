3d3c6c7f15d2d2ca8b417e8e2a9959e98b7a8d69
YinGang <mr.yingang.hb@gmail.com>
2016-12-13 14:37:52 +0800

    init

---
+++ main.js
@@ -0,0 +1,36 @@
+var electron = require('electron'),
+    path = require('path'),
+    url = require('url'),
+    app = electron.app,
+    BrowserWindow = electron.BrowserWindow;
+
+let mainWin;
+
+app.on('ready', createWindow);
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('activate', function () {
+  if (mainWin === null) {
+    createWindow()
+  }
+});
+
+
+function createWindow () {
+    mainWin = new BrowserWindow({width: 800, height: 600});
+
+    mainWin.loadURL(url.format({
+        pathname: path.join(__dirname, 'app','index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWin.webContents.openDevTools();
+
+    mainWin.on('closed', function () {
+        mainWin = null
+    });
+}
