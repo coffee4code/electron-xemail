5ddc9d7fc7ae435a3eb0a9464d33d2c2a247eedc
YinGang <mr.yingang.hb@gmail.com>
2016-12-19 15:11:15 +0800

    database api docs

--- app/js/app.js
+++ app/js/app.js
@@ -492,6 +492,12 @@ angular
             create: create
         };
 
+        /**
+         * Create database with sql, by default table name will be database name
+         * @param database
+         * @param sql
+         * @returns {boolean}
+         */
         function create(database, sql) {
 
             if(DB && DB[database]){
@@ -502,13 +508,21 @@ angular
                 db.run(sql);
                 DB[database] = db;
                 _commit(database);
+                return true;
             }
+            return false;
         }
 
+        /**
+         * Query data by filtered sql with filter data
+         * @param database
+         * @param string
+         * @param filter
+         * @returns {*}
+         */
         function filter(database, string, filter) {
             var data = null;
-            _open(database);
-            if(_isopen(database)){
+            if(_open(database) && _isopen(database)){
                 var db = DB[database];
                 var stmt = db.prepare(string);
                 data = stmt.getAsObject(filter);
@@ -516,10 +530,15 @@ angular
             return data;
         }
 
+        /**
+         * Execute sql and return result
+         * @param database
+         * @param sql
+         * @returns {*}
+         */
         function execute(database, sql) {
             var data = null;
-            _open(database);
-            if(_isopen(database)){
+            if(_open(database) && _isopen(database)){
                 var db = DB[database];
                 data = db.exec(sql);
                 _commit(database);
@@ -527,39 +546,86 @@ angular
             return data;
         }
 
+        /**
+         * Query all data in a table
+         * @param database
+         * @returns {*}
+         */
         function table(database) {
             var data = null;
-            _open(database);
-            if(_isopen(database)) {
+            if(_open(database) && _isopen(database)) {
                 var db = DB[database];
                 data = db.exec("SELECT * FROM "+ database);
             }
             return data;
         }
 
+        /**
+         * Check database exist status
+         * @param database
+         * @returns {*}
+         * @private
+         */
         function _exist(database) {
             return fs.existsSync(_getDbPath(database));
         }
 
+        /**
+         * Check database open status
+         * @param database
+         * @returns {Array|*}
+         * @private
+         */
         function _isopen(database) {
             return DB && DB[database] && DB[database]['db'];
         }
 
+        /**
+         * Open database
+         * @param database
+         * @returns {boolean}
+         * @private
+         */
         function _open(database) {
             if(_isopen(database)){
-                return;
+                return true;
             }
-            var fileBuffer = fs.readFileSync(_getDbPath(database));
-            DB[database] = new SQL.Database(fileBuffer);
+            try {
+                var fileBuffer = fs.readFileSync(_getDbPath(database));
+                DB[database] = new SQL.Database(fileBuffer);
+                return true;
+            } catch (e) {
+            } finally {
+                return false;
+            }
+
         }
+
+        /**
+         * Export and persistent database data
+         * @param database
+         * @private
+         */
         function _commit(database) {
-            if(_isopen(database)) {
-                var db = DB[database],
-                    data = db.export(),
-                    buffer = new Buffer(data);
-                fs.writeFileSync(_getDbPath(database), buffer);
+            try {
+                if(_isopen(database)) {
+                    var db = DB[database],
+                        data = db.export(),
+                        buffer = new Buffer(data);
+                    fs.writeFileSync(_getDbPath(database), buffer);
+                }
+            }catch (e){
+
+            }finally {
+                return ;
             }
         }
+
+        /**
+         * Get database file full path
+         * @param database
+         * @private
+         */
         function _getDbPath(database) {
             return path.resolve(__dirname, 'database', database)
         }
